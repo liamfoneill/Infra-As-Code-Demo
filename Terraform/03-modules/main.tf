e669395e7b60961d18fb7a97b89ca410a7da4221
Liam F. O'Neill <liam.f.oneill@microsoft.com>
2023-06-19 17:25:44 +0100

    add login details

--- Terraform/03-modules/main.tf
+++ Terraform/03-modules/main.tf
@@ -18,6 +18,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = "${env.ARM_SUBSCRIPTION_ID}"
+  client_id       = "${env.ARM_CLIENT_ID}"
+  client_secret   = "${env.ARM_CLIENT_SECRET}"
+  tenant_id       = "${env.ARM_TENANT_ID}"
 }
 
 module "health_storage_module" {
