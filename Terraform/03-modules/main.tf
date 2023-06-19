7880d24d29669583a3a20c0f085fc8f24ab1b924
Liam F. O'Neill <liam.f.oneill@microsoft.com>
2023-06-19 17:31:41 +0100

    remove un-needed config

--- Terraform/03-modules/main.tf
+++ Terraform/03-modules/main.tf
@@ -19,10 +19,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "${env.ARM_SUBSCRIPTION_ID}"
-  client_id       = "${env.ARM_CLIENT_ID}"
-  client_secret   = "${env.ARM_CLIENT_SECRET}"
-  tenant_id       = "${env.ARM_TENANT_ID}"
 }
 
 module "health_storage_module" {
